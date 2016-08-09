9f065e315dee1e47cc48cb84246cc1eabb4663b7
Sven Schmit <schmit@stanford.edu>
2016-08-08 21:37:07 -0700

    Updated plots and removed plots not in paper

--- plotting.py
+++ plotting.py
@@ -12,18 +12,24 @@ def smooth_plot(x, y, axes, frac=0.1, **kwargs):
     axes.plot(lowess[:, 0], lowess[:, 1])
 
 
-def scatter_evo_population(outcomes, axes, **kwargs):
+def scatter_evo_population(outcomes, axes, showall=False, **kwargs):
     pop_profs = np.array(outcomes["pop_profile"]).T
     narms, steps = pop_profs.shape
 
-    for idx, row in enumerate(pop_profs[:-1]):
+    if not showall:
+        pop_profs = pop_profs[:-1]
+
+    for idx, row in enumerate(pop_profs):
         axes.scatterplot(row, **kwargs)
 
 
-def plot_evo_population(outcomes, axes, frac=0.1):
+def plot_evo_population(outcomes, axes, frac=0.1, showall=False):
     pop_profs = np.array(outcomes["pop_profile"]).T
     narms, steps = pop_profs.shape
 
-    for idx, row in enumerate(pop_profs[:-1]):
+    if not showall:
+        pop_profs = pop_profs[:-1]
+
+    for idx, row in enumerate(pop_profs):
         t = np.arange(len(row))
         smooth_plot(t, row, axes, frac)
--- util.py
+++ util.py
@@ -1,4 +1,5 @@
 import pickle
+import time
 
 def dump(obj, fname):
     """
